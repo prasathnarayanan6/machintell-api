5b27ca9643d296610c1124c4d5b2afec00746b1e
prasathnarayanan6 <prasathnarayanan6@gmail.com>
2024-03-12 15:58:02 +0530

    Working

--- server/controller/product-specifications/UpdateProductSpecsController.js
+++ server/controller/product-specifications/UpdateProductSpecsController.js
@@ -2,8 +2,9 @@ const updateproductSpecsModel = require('../../model/product-specifications-mode
 const UpdateProductSpecsController = async (req, res) => {
     try
     {
-        const {id, product_specs, product_unit, product_value} = req.query;
-        const result = await updateproductSpecsModel(product_id, product_specs, product_unit, product_value);
+        const {id} = req.params
+        const {product_specs, product_unit, product_value} = req.query;
+        const result = await updateproductSpecsModel(id, product_specs, product_unit, product_value);
         res.status(200).json({message: 'Product Added Successfully', data: result});
     }
     catch (error)
